ddf5fab5a9234bfa0c95857d88d560baca302a98
kmshah59 <kmshah_b23@ce.vjti.ac.in>
2024-08-20 14:14:57 +0530

    HI

---
+++ first.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Include the input-output stream library
+
+int main() {
+    std::cout << "Running First Github Commmands" << std::endl; // Output "Hello, World!" to the console
+    return 0; // Indicate that the program ended successfully
+}
+
