48d1f9745a96d3f974235874a4fdcd600689e1e0
kmshah59 <kmshah_b23@ce.vjti.ac.in>
2024-08-20 14:32:57 +0530

    Changed to Hello World

---
+++ new_file.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Include the input-output stream library
+using namespace std;
+int main() {
+    cout<< "Hello, World!" <<endl; // Output "Hello, World!" to the console
+    return 0; // Indicate that the program ended successfully
+}
